eae78a8513b0ae64f0758d8a2b38ea32032e1557
peayad <peayad@gmail.com>
2016-09-28 12:42:08 +0200

    ctm convertor
    da b3d ma 3amlna el ctm convertor ... 7aga 3ala nedeef ya3ni XD

---
+++ 3js/04/autoconvert_STL_to_CTM.py
@@ -0,0 +1,9 @@
+
+import os
+
+for x in range(1,7):
+    for y in range(1,7):
+        for z in range(1,7):
+            filename = "R" + str(x) + "_H" + str(y) + "_O" + str(z)
+            os.system("ctmconv C:\\Users\\ptr\\Assignments\\whiteRoom\\deployment\\peayad.github.io\\3js\\04\\stl\\" + filename + ".stl" + " C:\\Users\\ptr\\Assignments\\whiteRoom\\deployment\\peayad.github.io\\3js\\04\\ctm\\" + filename + ".ctm --method MG2 --level 9")
+            print(x,y,z)
