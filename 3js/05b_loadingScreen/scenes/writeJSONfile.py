ae287e866a5a4dc8d1e6f055d08b147d2fbe3be4
peayad <peayad@gmail.com>
2016-10-02 01:51:52 +0200

    loading screen
    now there is a loading screen while preparing JSON file that links to
    the CTM files ... a7a

--- 3js/05b_loadingScreen/scenes/writeJSONfile.py
+++ 3js/05b_loadingScreen/scenes/writeJSONfile.py
@@ -1,7 +1,7 @@
 
 head = '{"metadata":{"formatVersion":3.2,"type":"scene"},"urlBaseType":"relativeToHTML","objects":{"group": {"position":[0,0,0],"rotation":[0,0,0],"scale":[1,1,1],"visible": true,"children":{'
 geometry = []
-body = ',"mainCamera":{"type":"PerspectiveCamera","fov": 50,"aspect":1.33333,"near":1,"far":5000,"position":[0,0,250],"target":[0,0,0]},"light1":{"type":"PointLight","position":[0,200,0],"color":16777215,"intensity":1},"light2":{"type":"PointLight","position":[100,200,100],"color":16777215,"intensity":1},"light3":{"type":"PointLight","position":[-100, -200, -100],"color":16777215,"intensity":1}}}},"geometries":{'
+body = ',"mainCamera":{"type":"PerspectiveCamera","fov": 35,"aspect":1.33333,"near":1,"far":2000,"position":[0,0,200],"target":[0,0,0]},"light1":{"type":"PointLight","position":[0,200,0],"color":16777215,"intensity":1},"light2":{"type":"PointLight","position":[100,200,100],"color":16777215,"intensity":1},"light3":{"type":"PointLight","position":[-100, -200, -100],"color":16777215,"intensity":1}}}},"geometries":{'
 url = []
 tail = '},"materials":{"myMaterial":{"type":"MeshPhongMaterial","parameters":{"color":1401481,"emissive":468276,"side":"double","shading":"flat"}}},"defaults":{"bgcolor":[0,0,0],"bgalpha":1,"camera":"mainCamera"}}'
 
